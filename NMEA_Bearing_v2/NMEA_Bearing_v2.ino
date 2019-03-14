7ed4b3ba955450327fc0648326b3a5665ac923ee
David McDonald <towerofpower256@gmail.com>
2019-03-14 11:17:24 +1000

    Removed button support
    At this point, resetting would be just the same as resetting or power cycling the whole arduino; less complexity invovled.
    
    That, and the Reset function keeps getting called if there's nothing plugged into A0.

--- NMEA_Bearing_v2/NMEA_Bearing_v2.ino
+++ NMEA_Bearing_v2/NMEA_Bearing_v2.ino
@@ -13,28 +13,8 @@
  */
 
 #include <LiquidCrystal.h>
+#include <math.h>
 #include "NmeaParser.h"
-
-// Freetronics button stuff
-#define USING_BUTTONS             1  // Are the buttons actually being used?
-#define BUTTON_ADC_PIN            A0  // A0 is the button ADC input
-#define RIGHT_10BIT_ADC           0  // right
-#define UP_10BIT_ADC            145  // up
-#define DOWN_10BIT_ADC          329  // down
-#define LEFT_10BIT_ADC          505  // left
-#define SELECT_10BIT_ADC        741  // right
-#define BUTTONHYSTERESIS         10  // hysteresis for valid button sensing window
-//return values for ReadButtons()
-#define BUTTON_NONE               0  // 
-#define BUTTON_RIGHT              1  // 
-#define BUTTON_UP                 2  // 
-#define BUTTON_DOWN               3  // 
-#define BUTTON_LEFT               4  // 
-#define BUTTON_SELECT             5  // 
-
-byte buttonJustPressed  = false;         //this will be true after a ReadButtons() call if triggered
-byte buttonJustReleased = false;         //this will be true after a ReadButtons() call if triggered
-byte buttonWas          = BUTTON_NONE;   //used by ReadButtons() for detection of button events
 
 // Constants
 const char SPACE_CHAR = ' ';
@@ -49,11 +29,9 @@ const char MAGNETIC_BEARING_DISPLAY_CHAR = 'C'; // C for Compass
 const char TRUE_BEARING_NMEA_CHAR = 'T';
 const char TRUE_BEARING_DISPLAY_CHAR = 'T';
 
-
 // Working variables
 float bearingM = -1;
 float bearingT = -1;
-bool resetButtonDown = false;
 
 // Messages
 const char STARTUP_MSG[] = "Starting...";
@@ -79,10 +57,6 @@ void setup() {
   lcd.clear();
   lcd.print(STARTUP_MSG);
 
-  // Setup some button stuff
-  pinMode( BUTTON_ADC_PIN, INPUT );         //ensure A0 is an input
-  digitalWrite( BUTTON_ADC_PIN, LOW );      //ensure pullup is off on A0
-
   // Ready
   Serial.println("Ready!");
   UpdateLcd();
@@ -91,16 +65,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  // Button stuff
-  if (USING_BUTTONS) {
-    byte button = ReadButtons();
-    if (button == BUTTON_SELECT && !resetButtonDown) {
-      resetButtonDown = true;
-      Reset();
-    } else if (button != BUTTON_SELECT && resetButtonDown) {
-      resetButtonDown = false;
-    }
-  }
+  
 }
 
 // Function to run whenever there's a serial event on the hardware serial RX
@@ -169,7 +134,6 @@ void Reset() {
   UpdateLcd();
 }
 
-
 void UpdateLcd() {
   
   lcd.clear();
@@ -217,57 +181,4 @@ float ConstrainAngle(float x) {
   x = x - 180;
 
   return x;
-}
-
-// Read the buttons
-byte ReadButtons()
-{
-   unsigned int buttonVoltage;
-   byte button = BUTTON_NONE;   // return no button pressed if the below checks don't write to btn
-   
-   //read the button ADC pin voltage
-   buttonVoltage = analogRead( BUTTON_ADC_PIN );
-   //sense if the voltage falls within valid voltage windows
-   if( buttonVoltage < ( RIGHT_10BIT_ADC + BUTTONHYSTERESIS ) )
-   {
-      button = BUTTON_RIGHT;
-   }
-   else if(   buttonVoltage >= ( UP_10BIT_ADC - BUTTONHYSTERESIS )
-           && buttonVoltage <= ( UP_10BIT_ADC + BUTTONHYSTERESIS ) )
-   {
-      button = BUTTON_UP;
-   }
-   else if(   buttonVoltage >= ( DOWN_10BIT_ADC - BUTTONHYSTERESIS )
-           && buttonVoltage <= ( DOWN_10BIT_ADC + BUTTONHYSTERESIS ) )
-   {
-      button = BUTTON_DOWN;
-   }
-   else if(   buttonVoltage >= ( LEFT_10BIT_ADC - BUTTONHYSTERESIS )
-           && buttonVoltage <= ( LEFT_10BIT_ADC + BUTTONHYSTERESIS ) )
-   {
-      button = BUTTON_LEFT;
-   }
-   else if(   buttonVoltage >= ( SELECT_10BIT_ADC - BUTTONHYSTERESIS )
-           && buttonVoltage <= ( SELECT_10BIT_ADC + BUTTONHYSTERESIS ) )
-   {
-      button = BUTTON_SELECT;
-   }
-   //handle button flags for just pressed and just released events
-   if( ( buttonWas == BUTTON_NONE ) && ( button != BUTTON_NONE ) )
-   {
-      //the button was just pressed, set buttonJustPressed, this can optionally be used to trigger a once-off action for a button press event
-      //it's the duty of the receiver to clear these flags if it wants to detect a new button change event
-      buttonJustPressed  = true;
-      buttonJustReleased = false;
-   }
-   if( ( buttonWas != BUTTON_NONE ) && ( button == BUTTON_NONE ) )
-   {
-      buttonJustPressed  = false;
-      buttonJustReleased = true;
-   }
-   
-   //save the latest button value, for change event detection next time round
-   buttonWas = button;
-   
-   return( button );
 }
